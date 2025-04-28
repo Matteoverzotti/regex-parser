cc3bdc9e4ef680f2c474875e636dec3b229064db
Matteoverzotti <matteoalexandru2612@gmail.com>
2025-04-28 20:01:12 +0300

    Added star and union operators

--- src/helper/thompson.rs
+++ src/helper/thompson.rs
@@ -1,5 +1,5 @@
 use crate::nfa::NFA;
-use std::collections::HashMap;
+use std::{collections::HashMap, fmt::format};
 
 pub fn concat(nfa1: NFA, mut nfa2: NFA) -> NFA {
     let mut transitions = nfa1.transitions.clone();
@@ -34,4 +34,90 @@ pub fn concat(nfa1: NFA, mut nfa2: NFA) -> NFA {
         nfa2.final_states,
         transitions
     )
+}
+
+pub fn union(mut nfa1: NFA, mut nfa2: NFA) -> NFA {
+    let total_states = nfa1.states.len() + nfa2.states.len();
+    let start = String::from("q0");
+    let end = format!("q{}", total_states + 1);
+
+    nfa1 = nfa1.rename_states(1);
+    nfa2 = nfa2.rename_states(nfa1.states.len() + 1);
+
+    let mut transitions: HashMap<String, HashMap<char, Vec<String>>> = HashMap::new();
+    transitions.insert(start.clone(), {
+        let mut map = HashMap::new();
+        map.insert('\0', vec![nfa1.q0, nfa2.q0]);
+        map
+    });
+
+    for (state, map) in nfa1.transitions.into_iter().chain(nfa2.transitions.into_iter()) {
+        transitions.insert(state, map);
+    }
+
+    for final_state in nfa1.final_states {
+        transitions.entry(final_state).or_insert_with(HashMap::new)
+            .entry('\0').or_insert_with(Vec::new)
+            .push(end.clone());
+    }
+
+    for final_state in nfa2.final_states {
+        transitions.entry(final_state).or_insert_with(HashMap::new)
+            .entry('\0').or_insert_with(Vec::new)
+            .push(end.clone());
+    }
+
+    let mut states = vec![start.clone(), end.clone()];
+    states.extend(nfa1.states);
+    states.extend(nfa2.states);
+
+    let mut sigma = nfa1.sigma;
+    sigma.extend(nfa2.sigma);
+    sigma.sort();
+    sigma.dedup();
+
+    NFA {
+        sigma,
+        states,
+        q0: start,
+        final_states: vec![end],
+        transitions,
+    }
+}
+
+pub fn star(mut nfa: NFA) -> NFA {
+    let start = String::from("q0");
+    let end = format!("q{}", nfa.states.len() + 1);
+
+    nfa = nfa.rename_states(1);
+
+    let mut transitions = HashMap::new();
+    transitions.insert(start.clone(), {
+        let mut map = HashMap::new();
+        map.insert('\0', vec![nfa.q0.clone(), end.clone()]);
+        map
+    });
+
+    for (state, map) in nfa.transitions {
+        transitions.insert(state, map);
+    }
+
+    for final_state in nfa.final_states {
+        transitions.entry(final_state).or_insert_with(HashMap::new)
+            .entry('\0').or_insert_with(Vec::new)
+            .extend(vec![nfa.q0.clone(), end.clone()]);
+    }
+
+    let mut states = vec![start.clone(), end.clone()];
+    states.extend(nfa.states);
+
+    let sigma = nfa.sigma;
+
+    NFA {
+        sigma,
+        states,
+        q0: start,
+        final_states: vec![end],
+        transitions,
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,16 @@ fn main() {
     let postfix = parser::to_postfix(tokens);
     println!("Postfix: {:?}", postfix);
 
-    let nfa1: NFA = NFA::from_char('\0');
+    let nfa1: NFA = NFA::from_char('a');
     nfa1.visualize();
 
-    let nfa2 = NFA::from_char('y');
+    let nfa2 = NFA::from_char('b');
     nfa2.visualize();
 
-    let nfa3 = thompson::concat(nfa1, nfa2);
-    let nfa4 = thompson::concat(nfa3.clone(), nfa3.clone());
+    let nfa3 = thompson::union(nfa1, nfa2);
+    nfa3.visualize();
+
+    let nfa4 = thompson::star(nfa3);
     nfa4.visualize();
+    
 }
